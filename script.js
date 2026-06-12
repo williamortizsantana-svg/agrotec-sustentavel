b90842d6e5714960fc5a2a91fe62b8d14ad48c75
williamortizsantana-svg <william.ortiz.santana@escola.pr.gov.br>
2026-06-12 14:29:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+function calcularEconomia() {
+
+    let consumo =
+        parseFloat(document.getElementById("consumo").value);
+
+    if (isNaN(consumo) || consumo <= 0) {
+
+        document.getElementById("resultado").innerHTML =
+        "Digite um valor válido.";
+
+        return;
+    }
+
+    let economia = consumo * 0.30;
+
+    document.getElementById("resultado").innerHTML =
+    "Com tecnologias sustentáveis, a economia estimada é de aproximadamente " +
+    economia.toFixed(0) +
+    " litros de água por mês.";
+}
